08092106678b2982c694f4e746b1812cdda023ba
Gaurav Malik <39573289+gauravmalik1388@users.noreply.github.com>
2024-10-23 04:29:09 -0400

    Intial commit for selenium

---
+++ csharp/seleniumtests/SeleniumTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace seleniumtests;
+
+public class SeleniumTests
+{
+[Test]
+public void FirstTest() {
+    IWebDriver driver = new ChromeDriver();
+    driver.Navigate().GoToUrl("https://www.selenium.dev/");
+    Assert.That(driver.Title, Is.EqualTo("Selenium"));
+    driver.Quit();
+}
+
+}
